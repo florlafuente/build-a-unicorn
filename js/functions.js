0bc12a30703d9bb848ec16cabf2b5af1da475440
florlafuente <floripona@gmail.com>
2017-08-03 22:46:07 +0000

    validation ready

--- js/functions.js
+++ js/functions.js
@@ -1,19 +1,77 @@
+var mistakes = [];
+
 $(document).ready(function() {
+	//Initializes empty array for validation
+		
+
 	 //Initialize Select Dropdown
     $('select').material_select();
 
     //Activates Burger Menu in Mobile Layout
-    //$(".button-collapse").sideNav();
+  	$('.button-collapse').sideNav();
 
-  $('.button-collapse').sideNav();
+    //Shows Alert Icon
+   	$('#mail').on('focusout', function(){
+   	if ($("#mail").hasClass('invalid')) {
+   		$("#error").css('display', 'block');
+   	}
+   }); //Ends Show Alert
 
-    $('.checkbox').on('click', function() {
-    	console.log('Hola');
-    });
+   	//Calls Validate Function When Button Gets Clicked
+   	$('#boton').on('click', function(){
+   		$('.message').css('display', 'none');
+   		validate();
+   		if (mistakes > 0) {
+		$('#fail').css('display', 'block');
+		mistakes = [];
+   		}else{
+   			var datos = $('#myform').serialize();
+   			$('#success').css('display', 'block');
+   			mistakes = [];
+   		}
+   		});
 
-   console.log('chau');
-    
 
- });
+   		
+
+});
+//function validate content 
+function validate() {
+	var name = $('#name').val();
+	var gender = $('#gender option:selected').val();
+	var color = $('#fur-color option:selected').val();
+	var horn = parseInt($('#horn').val());
+	var email = $('#mail').val();
+	var checkbox = $('#terms').is(':checked');
+
+	if (name.length == 0 || name == '') {
+		mistakes ++;
+	}
+
+	if (email.length == 0 ||email == '') {
+		mistakes ++;
+	}
+
+	if (gender == 0) {
+		mistakes ++;
+	}
+
+	if (color == 0) {
+		mistakes ++;
+
+	}
+
+	if (horn == 0) {
+		mistakes ++;
+	}
+
+	if (!checkbox) {
+		mistakes ++;
+	}
+}
+
+
+
+
 
 
